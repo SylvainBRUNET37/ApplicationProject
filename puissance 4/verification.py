149f76d4a316236728af0c97ac3e15f2e4967bd7
ChartonMatthieu <153715941+ChartonMatthieu@users.noreply.github.com>
2023-12-20 09:41:52 +0100

    verif

--- puissance 4/verification.py
+++ puissance 4/verification.py
@@ -1,4 +1,42 @@
-def colonne(tabPlateauDeJeu, iNbColones, iNbLignes, iNbJetonsVictoire):
-    for iBoucleC in range(iNbColones):
-        for iBoucleL in range(iNbLignes):
+def verifColonne(iTabPlateauDeJeu, iNbColonne, iNbLigne, iNbJetonVictoire):
+    tabVerif = []
+    for iBoucleC in range(iNbColonne):
+        for iBoucleL in range(iNbLigne):
+            iJeton = iTabPlateauDeJeu[iBoucleC][iBoucleL]
+            if len(tabVerif) == iNbJetonVictoire:
+                return tabVerif[0]
+            elif iJeton == tabVerif[0]:
+                tabVerif.append(iJeton)
+            else:
+                tabVerif = [iJeton]
+    return(0)
+
+def verifLigne(iTabPlateauDeJeu, iNbColonne, iNbLigne, iNbJetonVictoire):
+    tabVerif = []
+    for iBoucleL in range(iNbLigne):
+        for iBoucleC in range(iNbColonne):
+            iJeton = iTabPlateauDeJeu[iBoucleC][iBoucleL]
+            if len(tabVerif) == iNbJetonVictoire:
+                return tabVerif[0]
+            elif iJeton == tabVerif[0]:
+                tabVerif.append(iJeton)
+            else:
+                tabVerif = [iJeton]
+    return(0)
+
+def verifDiagonaleGauche(iTabPlateauDeJeu, iNbColonne, iNbLigne, iNbJetonVictoire):
+    for iBoucleC in range(iNbColonne - iNbJetonVictoire + 1):
+        for iBoucleL in range(iNbLigne - iNbJetonVictoire + 1):
+            iJeton = iTabPlateauDeJeu[iBoucleC][iBoucleL]
             
+    return 0
+
+
+def verifRemplie(iTabPlateauDeJeu, iNbColonne, iNbLigne):
+    for iBoucleC in range(iNbColonne):
+        for iBoucleL in range(iNbLigne):
+            if iTabPlateauDeJeu[iBoucleC][iBoucleL] == 0:
+                return(0)
+    return(3)
+
+a = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
