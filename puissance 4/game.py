ded4610f17c67014955c1a71039a01634b36957f
ChartonMatthieu <153715941+ChartonMatthieu@users.noreply.github.com>
2023-12-20 08:35:24 +0100

    code puissance4

---
+++ puissance 4/game.py
@@ -0,0 +1,10 @@
+def taille(Sizeh,Sizew):
+    print(Sizew,Sizeh)
+    
+def Game():
+    Sizeh=int(input("taille h"))
+    Sizew=int(input("taille w"))
+    gamebord = [[0]*Sizeh,[0*Sizew]]
+    taille(Sizeh,Sizew)
+
+Game()
---
+++ puissance 4/verification.py
@@ -0,0 +1,4 @@
+def colonne(tabPlateauDeJeu, iNbColones, iNbLignes, iNbJetonsVictoire):
+    for iBoucleC in range(iNbColones):
+        for iBoucleL in range(iNbLignes):
+            
