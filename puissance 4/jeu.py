134cd5bbb4e9ced66217c481db6b86ea306f77ea
Karonash <karo.game37@gmail.com>
2023-12-22 12:42:43 +0100

    typing et commentage

--- puissance 4/jeu.py
+++ puissance 4/jeu.py
@@ -1,39 +1,88 @@
-def identifierColonneChoisi(iTabColonne, iNbLigne):
+"""
+    @file jeu.py
+    @brief   Contient les fonctions permettant de jouer au jeu
+    @author  Sylvain BRUNET & Matthieu CHARTON
+    @version 0.1
+    @date    2023-2024
+"""
+
+"""
+    @brief  Identifie la ligne où devra être posé le jeton à partir de la colonne que le joueur a choisi
+    @param  iTabColonne colonne où le joueur veut placer son jeton
+    @param  iNbLigne    nombre de ligne du plateau de jeu
+    @return 'None' si la colonne est pleine, l'indice de la ligne où le jeton doit être posé si ce n'est pas le cas
+"""
+def identifierColonneChoisi(iTabColonne: int, iNbLigne: int) -> int:
+    # Parcours la colonne de bas en haut
     for iBoucleL in range(iNbLigne) :
+        # Si une ligne vide est trouvée, renvoie son indice
         if iTabColonne[iBoucleL] == 0 :
             return iBoucleL
     return None
 
-def placerJeton(iTabPlateauJeu, iJoueur, iNbColonne, iNbLigne):
-    bColonneValide = True
+"""
+    @brief  Demande au joueur la colonne où il veut placer son jeton puis le place
+    @param  iTabPlateauJeu plateau de jeu dans lequel le jeton doit être placé
+    @param  iJoueur        joueur qui veut placer le jeton
+    @param  iNbColonne     nombre de colonne du plateau de jeu
+    @param  iNbLigne       nombre de ligne du plateau de jeu
+    @return le plateau de jeu avec le jeton posé
+"""
+def placerJeton(iTabPlateauJeu: int, iJoueur: int, iNbColonne: int, iNbLigne: int) -> int:
+    bColonneValide: bool = True
     while bColonneValide == True :
         iColonneChoisi = int(input("Saisissez le numero de la colonne ou vous voulez jouer :"))
+        # Si l'indice de colonne saisi correspond bien a une ligne bien dans le plateau, verifie si la colonne associée est pleine
         if iColonneChoisi <= iNbColonne and iColonneChoisi >= 0 :
             iLigneJouer = identifierColonneChoisi(iTabPlateauJeu[iColonneChoisi], iNbLigne)
+            # Si la colonne n'est pas pleine, sort de la boucle (sinon, repart au début)
             if iLigneJouer != None :
                 bColonneValide = False
 
     iTabPlateauJeu[iColonneChoisi][iLigneJouer] = iJoueur
     return iTabPlateauJeu
-
-def demanderPositionPlateau(iTabPlateauJeu, iNbColonne, iNbLigne):
-    bPostionValide = True
-    while bPostionValide == True :
+"""
+    @brief  Demande au joueur la position d'un jeton et vérifie si il est bien dans le plateau
+    @param  iTabPlateauJeu plateau de jeu où le jeton doit être trouvé
+    @param  iNbColonne     nombre de colonne du plateau de jeu
+    @param  iNbLigne       nombre de ligne du plateau de jeu
+    @return indice de la colonne où doit être posé le jeton
+            indice de la ligne où doit être posé le jeton
+"""
+def demanderPositionPlateau(iTabPlateauJeu: int, iNbColonne: int, iNbLigne: int) -> int & int:
+    bPostionValide: bool = False
+    while bPostionValide == False :
         iColonneChoisi = int(input("Saisissez le numero de la colonne ou vous voulez retirer le jeton :"))
+        # Si l'indice de colonne saisi correspond bien a une ligne bien dans le plateau
         if iColonneChoisi <= iNbColonne and iColonneChoisi >= 0 :
             iLigneChoisi = int(input("Saisissez le numero de la ligne ou vous voulez retirer le jeton :"))
+            # Si l'indice de ligne saisi correspond bien a une ligne bien dans le plateau
             if iLigneChoisi <= iNbLigne and iLigneChoisi >= 0 :
+                # Si la position du jeton est vide (0), sort de la boucle (sinon, repart au début)
                 if (iTabPlateauJeu[iColonneChoisi][iLigneChoisi] != 0) :
-                    bPostionValide = False
+                    bPostionValide = True
     return iColonneChoisi, iLigneChoisi
 
-def retirerJeton(iTabPlateauJeu, iNbColonne, iNbLigne):
+"""
+    @brief  Retire un pion du plateau de jeu
+    @param  iTabPlateauJeu plateau de jeu où le jeton doit retiré
+    @param  iNbColonne     nombre de colonne du plateau de jeu
+    @param  iNbLigne       nombre de ligne du plateau de jeu
+    @return plateau de jeu avec le jeton retiré
+"""
+def retirerJeton(iTabPlateauJeu: int, iNbColonne: int, iNbLigne: int) -> int:
     iColonneChoisi, iLigneChoisi = demanderPositionPlateau(iTabPlateauJeu, iNbColonne, iNbLigne)
+    # Suprime le jeton à la position donnée
     iTabPlateauJeu[iColonneChoisi][iLigneChoisi] = 0
-    iBoucleL = iLigneChoisi+1
-    while iBoucleL < iNbLigne :
+    # Initialise la boucle à la position du jeton au dessus du jeton retiré
+    iBoucleL: int = iLigneChoisi+1
+    bFinBoucle: bool = False
+    # Boucle tant qu'il faut descendre des jetons
+    while iBoucleL < iNbLigne and bFinBoucle == False:
+        # Si il n'y avait pas de jeton au dessus, sort de la boucle
         if iTabPlateauJeu[iColonneChoisi][iBoucleL] == 0 :
-            break
+            bFinBoucle = True
+        # Sinon, déplace/fait tomber le jeton en dessous
         else :
             iTabPlateauJeu[iColonneChoisi][iBoucleL-1] = iTabPlateauJeu[iColonneChoisi][iBoucleL]
             iTabPlateauJeu[iColonneChoisi][iBoucleL] = 0
--- puissance 4/main.py
+++ puissance 4/main.py
@@ -3,27 +3,33 @@ from jeu import *
 from verification import *
 import os
 
-def cls():
+"""
+    @brief Efface la console
+"""
+def cls() -> None:
     os.system('cls' if os.name=='nt' else 'clear')
 
-def main():
-    iNbColonne = 7
-    iNbLigne = 6
-    iTabPlateauDeJeu = [[0 for i in range(iNbLigne)] for i in range(iNbColonne)]
-    tabStatutJeu = [[iTabPlateauDeJeu, True, True, 0]] # plateau, statut de l'atout du J1, statut de l'atout du J2, nombre de jeton dans la grille
+"""
+    @brief Contient la boucle principale du jeu et l'initialisation des données
+"""
+def main() -> None:
+    iNbColonne: int = 7
+    iNbLigne: int = 6
+    iTabPlateauDeJeu: int & int = [[0 for i in range(iNbLigne)] for i in range(iNbColonne)]
+    tabStatutJeu: int & int & int & int = [[iTabPlateauDeJeu, 1, 1, 0]] # plateau, statut de l'atout du J1, statut de l'atout du J2, nombre de jeton dans la grille
     
-    iJoueurCourant = 1
-    iBoucleJeu = 0
+    iJoueurCourant: int = 1
+    iBoucleJeu: int = 0
     while iBoucleJeu < 10 :
         afficherPlateau(tabStatutJeu[iBoucleJeu][0], iNbColonne, iNbLigne)
         tabStatutJeu.append(tabStatutJeu[iBoucleJeu]) # Met les données du tour précédent dans le tour actuelle
 
         # Si le joueur courant a encore son coup spécial, lui demande si il veut l'utiliser
-        if tabStatutJeu[iBoucleJeu][3] != 0 and tabStatutJeu[iBoucleJeu][iJoueurCourant] == True :
+        if tabStatutJeu[iBoucleJeu][3] != 0 and tabStatutJeu[iBoucleJeu][iJoueurCourant] != 0 :
             bRetirerJeton = int(input("Saisissez si vous 0 souhaitez utiliser votre coup special sinon 1 :")) # 0 = oui, 1 = non
             if bRetirerJeton == 0 :
                 tabStatutJeu[iBoucleJeu][0] = retirerJeton(tabStatutJeu[iBoucleJeu][0], iNbColonne, iNbLigne)
-                tabStatutJeu[iBoucleJeu][iJoueurCourant] = False # Enleve la posibilité d'utiliser l'atout du joueur qui l'a utilisé
+                tabStatutJeu[iBoucleJeu][iJoueurCourant] -= 1 # Retire une utilisation du coup spécial
                 tabStatutJeu[iBoucleJeu][3] -= 1
             else :
                 tabStatutJeu[iBoucleJeu][0] = placerJeton(tabStatutJeu[iBoucleJeu][0], iJoueurCourant, iNbColonne, iNbLigne)
