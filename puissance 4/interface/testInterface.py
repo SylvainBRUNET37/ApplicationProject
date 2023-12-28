47a70b3e5eb0e2042121f8b72ceaa8fd2114348c
Karonash <karo.game37@gmail.com>
2023-12-28 11:04:13 +0100

    test interfaces

--- puissance 4/interface/testInterface.py
+++ puissance 4/interface/testInterface.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import tkinter as tk
 
+def hide_button(widget): 
+        # This will remove the widget from toplevel 
+    widget.button_JvsJ.place_forget() 
+  
+  
+    # Method to make Button(widget) visible 
+def show_button(widget): 
+        # This will recover the widget from toplevel 
+    widget.button_JvsJ.place(anchor="nw", relx=0.1, rely=0.0, x=0, y=0)
 
-class PageparametreApp:
+
+class PagejeuApp:
     def __init__(self, master=None):
         # build ui
         self.pageParametre = tk.Tk() if master is None else tk.Toplevel(master)
@@ -31,112 +41,88 @@ class PageparametreApp:
             width=50)
         self.label_logo.place(anchor="nw", x=0, y=0)
         self.frame_haut.place(anchor="nw", x=10, y=5)
-        self.frame_couleurJ1 = tk.Frame(self.pageParametre)
-        self.frame_couleurJ1.configure(height=50, width=350)
-        self.label_couleurJ1 = tk.Label(self.frame_couleurJ1)
-        self.label_couleurJ1.configure(
-            font="{Arial} 16 {underline}",
-            text='Couleur de jeton J1 :')
-        self.label_couleurJ1.place(anchor="nw", relx=0.08, rely=0.27, x=0, y=0)
-        self.canva_couleurJ1 = tk.Canvas(self.frame_couleurJ1)
-        self.canva_couleurJ1.configure(
-            background="#8000ff",
-            closeenough=0,
-            confine=False,
-            cursor="pencil",
-            height=35,
-            highlightcolor="#ffff80",
-            width=35)
-        self.canva_couleurJ1.place(relx=0.75, rely=0.13, x=0, y=0)
-        self.frame_couleurJ1.place(anchor="nw", rely=0.20, x=17, y=0)
-        self.frame_couleurJ2 = tk.Frame(self.pageParametre)
-        self.frame_couleurJ2.configure(height=50, width=350)
-        self.label_couleurJ2 = tk.Label(self.frame_couleurJ2)
-        self.label_couleurJ2.configure(
-            font="{Arial} 16 {underline}",
-            text='Couleur de jeton J2 :')
-        self.label_couleurJ2.place(anchor="nw", relx=0.08, rely=0.27, x=0, y=0)
-        self.canva_couleurJ2 = tk.Canvas(self.frame_couleurJ2)
-        self.canva_couleurJ2.configure(
-            background="#8000ff",
-            closeenough=0,
-            confine=False,
-            cursor="pencil",
-            height=35,
-            width=35)
-        self.canva_couleurJ2.place(relx=0.75, rely=0.13, x=0, y=0)
-        self.frame_couleurJ2.place(anchor="nw", rely=0.35, x=17, y=0)
-        self.frame_nombreCoupSpecial = tk.Frame(self.pageParametre)
-        self.frame_nombreCoupSpecial.configure(height=50, width=350)
-        self.label15 = tk.Label(self.frame_nombreCoupSpecial)
-        self.label15.configure(
-            font="{Arial} 16 {underline}",
-            text='Nombre de coup \nspécial :')
-        self.label15.place(anchor="nw", relx=0.08, rely=0.0, x=0, y=0)
-        self.scale_nombreCoupSpecial = tk.Scale(self.frame_nombreCoupSpecial)
-        self.scale_nombreCoupSpecial.configure(
-            cursor="sb_h_double_arrow", from_=0, orient="horizontal", to=5)
-        self.scale_nombreCoupSpecial.place(
-            anchor="nw", relx=0.68, rely=0.1, x=0, y=0)
-        self.frame_nombreCoupSpecial.place(anchor="nw", rely=0.52, x=17, y=0)
-        self.frame_commence = tk.Frame(self.pageParametre)
-        self.frame_commence.configure(height=50, width=350)
-        self.label_commence = tk.Label(self.frame_commence)
-        self.label_commence.configure(
-            font="{Arial} 16 {underline}",
-            text='Joueur qui commence :')
-        self.label_commence.place(anchor="nw", relx=0.07, rely=0.0, x=0, y=0)
-        self.button_commenceJ1 = tk.Button(self.frame_commence)
-        self.button_commenceJ1.configure(
-            background="#ffffff",
+        self.frame_choixAdversaire = tk.Frame(self.pageParametre)
+        self.frame_choixAdversaire.configure(height=90, width=365)
+        self.button_JvsIA = tk.Button(self.frame_choixAdversaire)
+        self.button_JvsIA.configure(
             cursor="hand2",
+            font="{Arial} 16 {}",
             overrelief="sunken",
-            text='J1',
-            width=3)
-        self.button_commenceJ1.place(
-            anchor="nw", relx=0.76, rely=0.0, x=0, y=0)
-        self.button_commenceJ2 = tk.Button(self.frame_commence)
-        self.button_commenceJ2.configure(
-            background="#c0c0c0",
+            text='Joueur \nVS \nIA',
+            width=8)
+        self.button_JvsIA.place(anchor="nw", relx=0.6, rely=0.0, x=0, y=0)
+        self.button_JvsJ = tk.Button(self.frame_choixAdversaire)
+        self.button_JvsJ.configure(
             cursor="hand2",
+            font="{Arial} 16 {}",
             overrelief="sunken",
-            text='J2',
-            width=3)
-        self.button_commenceJ2.place(anchor="nw", relx=0.90, x=0, y=0)
-        self.frame_commence.place(anchor="nw", rely=0.72, x=17, y=0)
-        self.frame_diffculte = tk.Frame(self.pageParametre)
-        self.frame_diffculte.configure(height=50, width=350)
-        self.label_difficulte = tk.Label(self.frame_diffculte)
-        self.label_difficulte.configure(
-            cursor="pirate",
-            font="{Arial} 16 {underline}",
-            text="Difficulté de l'IA :")
-        self.label_difficulte.place(anchor="nw", relx=0.07, rely=0.0, x=0, y=0)
-        self.button_difficulte1 = tk.Button(self.frame_diffculte)
-        self.button_difficulte1.configure(
-            background="#ffffff",
-            cursor="hand2",
-            overrelief="sunken",
-            text='1',
-            width=3)
-        self.button_difficulte1.place(anchor="nw", relx=0.6, x=0, y=0)
-        self.button_difficulte2 = tk.Button(self.frame_diffculte)
-        self.button_difficulte2.configure(
-            background="#c0c0c0",
-            cursor="hand2",
-            overrelief="sunken",
-            text='2',
-            width=3)
-        self.button_difficulte2.place(anchor="nw", relx=0.75, x=0, y=0)
-        self.button_difficulte3 = tk.Button(self.frame_diffculte)
-        self.button_difficulte3.configure(
-            background="#c0c0c0",
-            cursor="hand2",
-            overrelief="sunken",
-            text='3',
-            width=3)
-        self.button_difficulte3.place(anchor="nw", relx=0.90, x=0, y=0)
-        self.frame_diffculte.place(anchor="nw", rely=0.85, x=17, y=0)
+            text='Joueur \nVS \nJoueur',
+            width=8,
+            command=hide_button(self))
+        self.button_JvsJ.place(anchor="nw", relx=0.1, rely=0.0, x=0, y=0)
+
+
+
+        self.frame_choixAdversaire.place(anchor="nw", rely=0.23, x=10, y=5)
+        self.frame_choixAdversaire.grid_propagate(0)
+        self.frame_hauteur = tk.Frame(self.pageParametre)
+        self.frame_hauteur.configure(height=75, width=200)
+        self.scale_hauteur = tk.Scale(self.frame_hauteur)
+        self.scale_hauteur.configure(
+            cursor="sb_h_double_arrow",
+            from_=3,
+            orient="horizontal",
+            relief="flat",
+            to=10)
+        self.scale_hauteur.place(anchor="nw", relx=0.18, rely=0.0, x=0, y=0)
+        self.label_hauteur = tk.Label(self.frame_hauteur)
+        self.label_hauteur.configure(text='Hauteur du plateau')
+        self.label_hauteur.place(anchor="nw", relx=0.17, rely=0.52, x=0, y=0)
+        self.frame_hauteur.place(anchor="nw", rely=0.55, x=10, y=5)
+        self.frame_largeur = tk.Frame(self.pageParametre)
+        self.frame_largeur.configure(height=75, width=200)
+        self.scale_largeur = tk.Scale(self.frame_largeur)
+        self.scale_largeur.configure(
+            cursor="sb_h_double_arrow",
+            from_=3,
+            orient="horizontal",
+            relief="flat",
+            to=10)
+        self.scale_largeur.place(anchor="nw", relx=0.135, rely=0.0, x=0, y=0)
+        self.label_largeur = tk.Label(self.frame_largeur)
+        self.label_largeur.configure(text='Largeur du plateau')
+        self.label_largeur.place(anchor="nw", relx=0.135, rely=0.52, x=0, y=0)
+        self.frame_largeur.place(anchor="nw", relx=0.5, rely=0.55, x=10, y=5)
+        self.frame_nombreJeton = tk.Frame(self.pageParametre)
+        self.frame_nombreJeton.configure(height=75, width=200)
+        self.scale_nombreJeton = tk.Scale(self.frame_nombreJeton)
+        self.scale_nombreJeton.configure(
+            cursor="sb_h_double_arrow",
+            from_=3,
+            orient="horizontal",
+            relief="flat",
+            to=6)
+        self.scale_nombreJeton.place(
+            anchor="nw", relx=0.18, rely=0.0, x=0, y=0)
+        self.label_nombreJeton = tk.Label(self.frame_nombreJeton)
+        self.label_nombreJeton.configure(text='Nombre de jeton à alligner')
+        self.label_nombreJeton.place(
+            anchor="nw", relx=0.1, rely=0.52, x=0, y=0)
+        self.frame_nombreJeton.place(
+            anchor="nw", relx=0.0, rely=0.75, x=10, y=5)
+        self.frame_checkbutton = tk.Frame(self.pageParametre)
+        self.frame_checkbutton.configure(height=75, width=200)
+        self.checkbutton_undoRedo = tk.Checkbutton(self.frame_checkbutton)
+        self.checkbutton_undoRedo.configure(cursor="hand2", text='Undo / Redo')
+        self.checkbutton_undoRedo.place(
+            anchor="nw", relx=0.14, rely=0.43, x=0, y=0)
+        self.checkbutton_coupSpecial = tk.Checkbutton(self.frame_checkbutton)
+        self.checkbutton_coupSpecial.configure(
+            cursor="hand2", text='Coup special')
+        self.checkbutton_coupSpecial.place(
+            anchor="nw", relx=0.14, rely=0.15, x=0, y=0)
+        self.frame_checkbutton.place(
+            anchor="nw", relx=0.5, rely=0.75, x=10, y=5)
 
         # Main widget
         self.mainwindow = self.pageParametre
@@ -146,6 +132,6 @@ class PageparametreApp:
 
 
 if __name__ == "__main__":
-    app = PageparametreApp()
+    app = PagejeuApp()
     app.run()
 
