d47134ad1c18f76f9ac922a8f630a6d89e2dbf2c
Karonash <karo.game37@gmail.com>
2024-01-09 09:26:34 +0100

    changement interface principale
    définition de fonction pour "dispatcher" le code

--- puissance 4/interfacePrincipale.py
+++ puissance 4/interfacePrincipale.py
@@ -1,39 +1,72 @@
 #!/usr/bin/python3
 import tkinter as tk
+from typing import *
 
-def main(master=None):
-    # build ui
-    pageParametre = tk.Tk()
-    pageParametre.configure(height=200, width=200)
-    pageParametre.geometry("384x288")
-    frame_haut = tk.Frame(pageParametre)
-    frame_haut.configure(height=50, width=365)
+"""
+    @brief  Créé une fenêtre
+    @param  iLargeurFenetre   largeur de la fenêtre
+    @param  iHauteurFenetre   hauteur de la fenêtre
+    @param  bRedimensionnable si égal a True, la fenêtre sera redimensionnable (et inversement pour False)
+    @param  sNomFenetre       nom de la fenêtre
+    @return la fenêtre créée
+"""
+def creerToplevelFenetre(iLargeurFenetre : int, iHauteurFenetre : int, bRedimensionnable : bool, sNomFenetre : str) -> tk:
+    toplevelFenetre = tk.Tk()
 
-    button_quitter = tk.Button(frame_haut)
-    button_quitter.configure(cursor="hand2", font="{Arial} 16 {}", overrelief="sunken", text='Quitter')
-    button_quitter.place(anchor="nw", relx=0.78, rely=0.04, x=0, y=0)
-    label_nomJeu = tk.Label(frame_haut)
-    label_nomJeu.configure(font="{Arial} 20 {bold underline}",text='Puissance N')
-    label_nomJeu.place(anchor="nw", relx=0.23, rely=0.04, x=0, y=0)
+    # Recupère les dimensions (largeur et hauteur) de l'écran de l'utilisateur
+    iLargeurEcran : int = toplevelFenetre.winfo_screenwidth()
+    iHauteurEcran : int = toplevelFenetre.winfo_screenheight()
+ 
+    # Calcule des coodonnées où devra être placer la fenêtre pour être au centre de l'écran
+    iCoordonneeFenetreX : int = int((iLargeurEcran/2) - (iLargeurFenetre/2))
+    iCoordonneeFenetreY : int = int((iHauteurEcran/2) - (iHauteurFenetre/2))
+ 
+    # Applique les dimensions à la fenêtre et la place aux coodonnées calculées précédement
+    toplevelFenetre.geometry("{}x{}+{}+{}".format(iLargeurFenetre, iHauteurFenetre, iCoordonneeFenetreX, iCoordonneeFenetreY))
+    toplevelFenetre.resizable(bRedimensionnable, bRedimensionnable)
+    toplevelFenetre.title(sNomFenetre)
 
-    label_logo = tk.Label(frame_haut)
-    img_logo = tk.PhotoImage(file="interface/logo.png")
-    label_logo.configure(cursor="coffee_mug", height=50, image=img_logo,width=50)
-    label_logo.place(anchor="nw", x=0, y=0)
+    return toplevelFenetre
 
-    frame_haut.place(anchor="nw", x=10, y=5)
-    frame_choixAdversaire = tk.Frame(pageParametre)
+"""
+    @brief  Créé la haut des pages : le logo du jeu, le titre et le bouton quitter
+    @param  toplevelFenetre la fenêtre où placer le haut de page
+"""    
+def creerFrameHaut(toplevelFenetre : tk):
+    frameHaut = tk.Frame(toplevelFenetre)
+    frameHaut.configure(height=100, width=750)
+    frameHaut.place(anchor="nw", x=10, y=5)
+
+    labelLogo = tk.Label(frameHaut)
+    imageLogo = tk.PhotoImage(file="interface/logo.png")
+    labelLogo.configure(image=imageLogo, height=85, width=85, background="yellow")
+    labelLogo.place(anchor="nw", relx=0.05, rely=0.15)
+
+    labelNomJeu = tk.Label(frameHaut)
+    labelNomJeu.configure(font="{Arial} 36 {bold underline}", text='Puissance N')
+    labelNomJeu.place(anchor="nw", relx=0.25, rely=0.20)
+
+    buttonQuitter = tk.Button(frameHaut)
+    buttonQuitter.configure(cursor="hand2", font="{Arial} 26 {}", width=10, text='Quitter', command=toplevelFenetre.destroy)
+    buttonQuitter.place(anchor="nw", relx=0.70, rely=0.20)
+
+
+def main():
+    toplevelPagePrincipale = creerToplevelFenetre(768, 576, False, "Page principale")    
+    creerFrameHaut(toplevelPagePrincipale)
+
+
+    frame_choixAdversaire = tk.Frame(toplevelPagePrincipale)
     frame_choixAdversaire.configure(height=90, width=365)
     button_JvsIA = tk.Button(frame_choixAdversaire)
-    button_JvsIA.configure(cursor="hand2", font="{Arial} 16 {}", overrelief="sunken", text='Joueur \nVS \nIA', width=8)
+    button_JvsIA.configure(cursor="hand2", font="{Arial} 16 {}", text='Joueur \nVS \nIA', width=8)
     button_JvsIA.place(anchor="nw", relx=0.6, rely=0.0, x=0, y=0)
     button_JvsJ = tk.Button(frame_choixAdversaire)
-    button_JvsJ.configure(cursor="hand2", font="{Arial} 16 {}", overrelief="sunken", text='Joueur \nVS \nJoueur', width=8)
+    button_JvsJ.configure(cursor="hand2", font="{Arial} 16 {}", text='Joueur \nVS \nJoueur', width=8)
     button_JvsJ.place(anchor="nw", relx=0.1, rely=0.0, x=0, y=0)
 
     frame_choixAdversaire.place(anchor="nw", rely=0.23, x=10, y=5)
-    frame_choixAdversaire.grid_propagate(0)
-    frame_hauteur = tk.Frame(pageParametre)
+    frame_hauteur = tk.Frame(toplevelPagePrincipale)
     frame_hauteur.configure(height=75, width=200)
     scale_hauteur = tk.Scale(frame_hauteur)
     scale_hauteur.configure(cursor="sb_h_double_arrow", from_=3,orient="horizontal", relief="flat", to=10)
@@ -42,7 +75,8 @@ def main(master=None):
     label_hauteur.configure(text='Hauteur du plateau')
     label_hauteur.place(anchor="nw", relx=0.17, rely=0.52, x=0, y=0)
     frame_hauteur.place(anchor="nw", rely=0.55, x=10, y=5)
-    frame_largeur = tk.Frame(pageParametre)
+    
+    frame_largeur = tk.Frame(toplevelPagePrincipale)
     frame_largeur.configure(height=75, width=200)
     scale_largeur = tk.Scale(frame_largeur)
     scale_largeur.configure(cursor="sb_h_double_arrow", from_=3, orient="horizontal", relief="flat", to=10)
@@ -51,7 +85,8 @@ def main(master=None):
     label_largeur.configure(text='Largeur du plateau')
     label_largeur.place(anchor="nw", relx=0.135, rely=0.52, x=0, y=0)
     frame_largeur.place(anchor="nw", relx=0.5, rely=0.55, x=10, y=5)
-    frame_nombreJeton = tk.Frame(pageParametre)
+    
+    frame_nombreJeton = tk.Frame(toplevelPagePrincipale)
     frame_nombreJeton.configure(height=75, width=200)
     scale_nombreJeton = tk.Scale(frame_nombreJeton)
     scale_nombreJeton.configure(cursor="sb_h_double_arrow",from_=3,orient="horizontal",relief="flat",to=6)
@@ -60,7 +95,8 @@ def main(master=None):
     label_nombreJeton.configure(text='Nombre de jeton à alligner')
     label_nombreJeton.place(anchor="nw", relx=0.1, rely=0.52, x=0, y=0)
     frame_nombreJeton.place(anchor="nw", relx=0.0, rely=0.75, x=10, y=5)
-    frame_checkbutton = tk.Frame(pageParametre)
+    
+    frame_checkbutton = tk.Frame(toplevelPagePrincipale)
     frame_checkbutton.configure(height=75, width=200)
     checkbutton_undoRedo = tk.Checkbutton(frame_checkbutton)
     checkbutton_undoRedo.configure(cursor="hand2", text='Undo / Redo')
@@ -69,7 +105,7 @@ def main(master=None):
     checkbutton_coupSpecial.configure(cursor="hand2", text='Coup special')
     checkbutton_coupSpecial.place(anchor="nw", relx=0.14, rely=0.15, x=0, y=0)
     frame_checkbutton.place(anchor="nw", relx=0.5, rely=0.75, x=10, y=5)
-    pageParametre.mainloop()
+    toplevelPagePrincipale.mainloop()
 
 
 main()
